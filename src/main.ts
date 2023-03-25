95116872a8bd851987890522d56a7f8fb42be378
Vishal D Bhat <60818841+vishalbhatd123456@users.noreply.github.com>
2023-03-25 15:29:47 +0000

    steps to create controller in nextjs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { NestFactory } from "@nestjs/core";
 
 //decorater that informs nest that the class
 //handles incoming requests
+//1. create a root controller
+// 2. create a module and mention the controller we defined in step 1
+//3. define and invode bootstrap function to listen to our traffic
 @Controller()
 class AppController{
     @Get()
@@ -21,7 +24,7 @@ class AppModule{
 
 //runs every time
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
+    const app = await NestFactory.create(AppModule) //instance of instance
     await app.listen(3000)
 }
 bootstrap();
